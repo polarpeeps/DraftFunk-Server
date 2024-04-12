460066ba99c4d8d29e409fbf3bb2d02f29c3fd7b
Nabeel <ali31hasan.com>
2024-04-13 00:07:31 +0530

    v5

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,19 @@ module.exports = {
         path:path.resolve(__dirname,'dist')
     },
     target:'node',
+    resolve: {
+        fallback: {
+            "kerberos": false,
+            "@mongodb-js/zstd": false,
+            "@aws-sdk/credential-providers": false,
+            "gcp-metadata": false,
+            "snappy": false,
+            "socks": false,
+            "aws4": false,
+            // Add any other modules you want to exclude here
+        },
+    },
+    
     module:{
         rules:[
             {
